b52359c9b5995c34a5871c513a91ac1c61cc35c6
Sparrow-Tech06 <cpal980075@gmail.com>
2026-06-02 17:05:56 +0530

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -36,15 +36,15 @@ document.addEventListener("DOMContentLoaded", () => {
                     <svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                     <span>Home</span>
                 </a>
-                <a href="likes.html" class="nav-item" data-page="category">
+                <a href="categories.html" class="nav-item" data-page="category">
                     <svg viewBox="0 0 24 24"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                     <span>Likes</span>
                 </a>
-                <a href="search.html" class="nav-item" data-page="search">
+                <a href="settings.html" class="nav-item" data-page="search">
                     <svg viewBox="0 0 24 24"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                     <span>Search</span>
                 </a>
-                <a href="profile.html" class="nav-item" data-page="setting">
+                <a href="settings.html" class="nav-item" data-page="setting">
                     <svg viewBox="0 0 24 24"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                     <span>Profile</span>
                 </a>
